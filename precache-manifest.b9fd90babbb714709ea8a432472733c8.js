018cbf22272bd31dc16259e9d7f7962bf6e6bc0f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-24 05:54:39 +0000

    Deploy mazipan/vue-doughnut-chart to github.com/mazipan/vue-doughnut-chart.git:gh-pages

---
+++ precache-manifest.b9fd90babbb714709ea8a432472733c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae7b457c9e6a4387022913d9152fce3",
+    "url": "/vue-doughnut-chart/index.html"
+  },
+  {
+    "revision": "1467aa82fdb596a494d32a21e85db5fa",
+    "url": "/vue-doughnut-chart/index.html.gz"
+  },
+  {
+    "url": "/vue-doughnut-chart/js/app.2f2f7060f0e69b7c17ac.js"
+  },
+  {
+    "url": "/vue-doughnut-chart/js/app.2f2f7060f0e69b7c17ac.js.gz"
+  },
+  {
+    "url": "/vue-doughnut-chart/js/vendors~app.2f2f7060f0e69b7c17ac.js"
+  },
+  {
+    "url": "/vue-doughnut-chart/js/vendors~app.2f2f7060f0e69b7c17ac.js.gz"
+  }
+]);
